326558a8a4c96e34d780090a125d5ab2de428d90
yukkop <hectic.yukkop@gmail.com>
2024-02-29 01:10:02 +0100

    feat: log level variable

--- sh/log.sh
+++ sh/log.sh
@@ -1,23 +1,45 @@
 #!/bin/sh
 
+# SH_LOG: info, error, warning, trace
+
 RED=$(printf '\033[31;1m')
 YELLOW=$(printf '\033[33;1m')
 MAGENTA=$(printf '\033[35;1m')
 BLUE=$(printf '\033[34;1m')
 RESET=$(printf '\033[0m')
 
+if [ $SH_LOG ]; then
+  SH_LOG="info"
+fi
+
+info() {
+  if [ $SH_LOG = "info" ] || [ $SH_LOG = "trace" ]; then
+    printf "%sINFO%s: %s\n" "${BLUE}" "${RESET}" "$*"
+  fi
+}
+
 log() {
-  printf "%sLOG%s: %s\n" "${BLUE}" "${RESET}" "$*"
+  info ${@:1}
 }
 
 error() {
-  printf "%sERROR%s: %s\n" "${RED}" "${RESET}" "$*"
+  if [ $SH_LOG = "info" ] || [ $SH_LOG = "trace" ] || [ $SH_LOG = "warn" ] || [ $SH_LOG = "error" ]; then
+    printf "%sERROR%s: %s\n" "${RED}" "${RESET}" "$*"
+  fi
+}
+
+warn() {
+  if [ $SH_LOG = "info" ] || [ $SH_LOG = "trace" ] || [ $SH_LOG = "warn" ]; then
+    printf "%sWARNING%s: %s\n" "${YELLOW}" "${RESET}" "$*"
+  fi
 }
 
 warning() {
-  printf "%sWARNING%s: %s\n" "${YELLOW}" "${RESET}" "$*"
+  warn ${@:1}
 }
 
 trace() {
-  printf "%sTRACE%s: %s\n" "${MAGENTA}" "${RESET}" "$*"
+  if [ $SH_LOG = "trace" ]; then
+    printf "%sTRACE%s: %s\n" "${MAGENTA}" "${RESET}" "$*"
+  fi
 }
--- sh/test/log.sh
+++ sh/test/log.sh
@@ -4,7 +4,36 @@ SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 . "$SCRIPT_DIR/../log.sh"
 TEXT=text
 
-log some log $TEXT 
-error some error $TEXT 
-warning some error $TEXT 
-trace some error $TEXT 
+SH_LOG="info"
+printf "info level\n"
+info log level $SH_LOG 
+error log level $SH_LOG 
+warn log level $SH_LOG 
+trace log level $SH_LOG 
+
+SH_LOG="warn"
+printf "\nwarn level\n"
+info log level $SH_LOG 
+error log level $SH_LOG 
+warn log level $SH_LOG 
+trace log level $SH_LOG 
+
+SH_LOG="error"
+printf "\nerror level\n"
+info log level $SH_LOG 
+error log level $SH_LOG 
+warn log level $SH_LOG 
+trace log level $SH_LOG 
+
+SH_LOG="trace"
+printf "\ntrace level\n"
+info log level $SH_LOG 
+error log level $SH_LOG 
+warn log level $SH_LOG 
+trace log level $SH_LOG 
+
+#Legacy test
+SH_LOG="trace"
+printf "\nlegacy level\n"
+log log level $SH_LOG 
+warning log level $SH_LOG 
